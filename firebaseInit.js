5ef4411a8c4909b330e529822f2f06e35877a2ef
nishat1 <nishatgupta1@gmail.com>
2018-01-13 15:33:18 -0800

    base file

---
+++ firebaseInit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDekimbKLKfVoPmo1zeE1MK2ppQlNjJ0Fc",
+    authDomain: "child-checkup.firebaseapp.com",
+    databaseURL: "https://child-checkup.firebaseio.com",
+    projectId: "child-checkup",
+    storageBucket: "",
+    messagingSenderId: "51982991127"
+  };
+  firebase.initializeApp(config);
