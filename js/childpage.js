1c0f042d874cd822f117ef92b676451d1f1ff164
Eric <ericduong515@gmail.com>
2018-01-14 11:39:07 -0800

    Done child page. May need to add forward slashes to directory references when integrated

--- js/childpage.js
+++ js/childpage.js
@@ -3,37 +3,62 @@ function savePatientInfo()
 	var device_num = document.getElementById("device-num").value;
 	var patient_name = document.getElementById("child-name").value;
 	var room_num = document.getElementById("room-num").value;
-	var success;
 	var init_form = document.getElementById("child-login");
 	var status_form = document.getElementById("child-status-form");
 	var logout_button = document.getElementById("logout-button");
+	
+	var today = new Date().toLocaleDateString('en-US', {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit'
+	});
+	
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+		if(firebaseUser){
+			console.log(firebaseUser);
+			
+			/* I should probably do a better job of error handling but this promise thing won't let me supress the success message */
+			initPatientStatus(device_num, patient_name, room_num, today);
+		}
+		else{
+			console.log("Not logged in");
+		}
+	});
 	
 	/* DB Authenticate */
 	const promise1 = firebase.auth().signInWithEmailAndPassword("a@a.com", "aaaaaa");
 	promise1.catch(e => console.log(e.message));
 	
-	/* I should probably do a better job of error handling but this promise thing won't let me supress the success message */
-	const promise2 = firebase.database().ref().child('devices2/' + device_num).set({
-		patient_name: patient_name,
-		status: "N/A",
-		room_num: room_num
-	});
-
-	/* Not ideal because of above reason */
-	promise2.catch(e => showSetupFailure());
-	
 	/* No need to show success message as it will auto load the next screen */
-	//showSetupSuccess();
+	showSetupSuccess();
 	
 	init_form.style.display = "none";
 	status_form.style.display = "block";
 	logout_button.style.display = "block";
+}
+function initPatientStatus(device_num, patient_name, room_num, time)
+{
+	const promise2 = firebase.database().ref().child('devices/' + device_num).set({
+		patient_name: patient_name,
+		status: "N/A",
+		room_num: room_num,
+		time: time
+	});
+	
+	/* Not ideal because of above reason */
+	promise2.catch(e => showSetupFailure());
 }
 
 function showSetupSuccess()
 {
 	var success_msg = document.getElementById("init-success");
 	success_msg.style.display = "block";
+	
+	setTimeout(function() {
+		$('#init-success').fadeOut('fast');
+	}, 2500);
 }
 
 function showSetupFailure()
@@ -61,38 +86,58 @@ function showStatusFailure()
 
 function updateChildStatus(child_status)
 {
+	var today = new Date().toLocaleDateString('en-US', {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit'
+	});
 	var device_num = document.getElementById("device-num").value;
-	const promise = firebase.database().ref('devices2/' + device_num).update({
-		status: child_status
+	const promise = firebase.database().ref('devices/' + device_num).update({
+		status: child_status,
+		time: today
 	});
 	promise.catch(e => showStatusFailure());
 	
-	showStatusSuccess();
-}
-
-function toggleNurse()
-{
-	//TODO
+	if(child_status == 'help')
+		showNurseSuccess();
+	else
+		showStatusSuccess();
 }
 
 function logout(){
+	var form_object = document.forms['init-child'];
+	var device_num = form_object.elements["device-num"].value;
+	var patient_name = form_object.elements['child-name'].value;
+	var room_num = form_object.elements['room-num'].value;
+	
+	// Wipe out old data. Does not actually work.
+	device_num = '';
+	patient_name = '';
+	room_num = '';
+	
 	document.getElementById('child-login').style.display = 'block';
 	document.getElementById('child-status-form').style.display = 'none';
 	document.getElementById('logout-button').style.display = 'none';
 	
-	/*
-	firebase.auth().signOut().then(function() {
-	  showLogoutSuccess();
-	}).catch(function(error) {
-	  console.log(error.message)
-	});
-	*/
+	updateChildStatus('N/A'); //Actually bugged due to onAuthStateChanged but insignificant
 	
+	firebase.auth().signOut();
+	
+	showLogoutSuccess();
 }
 
 function showLogoutSuccess(){
 	document.getElementById('logout-success').style.display = 'block';
 	setTimeout(function() {
 		$('#logout-success').fadeOut('fast');
+	}, 2500);
+}
+
+function showNurseSuccess(){
+	document.getElementById('nurse-success').style.display = 'block';
+	setTimeout(function() {
+		$('#nurse-success').fadeOut('fast');
 	}, 2500);
 }
