964afcef076fae258fe70b5e6b4621f2aac0eb76
Alec Mac <alec@Alecs-MacBook-Pro.local>
2018-01-13 21:25:39 -0800

    appending child info cards

--- js/parentPage.js
+++ js/parentPage.js
@@ -7,23 +7,32 @@ const child_status = document.getElementById('child_status');
 // const dbRef = firebase.database().ref().child('child');
 var deviceNum = 0;
 var dbRef = firebase.database().ref().child('devices');
-
-
+var childName, childRoom, childStatus;
 function getDeviceNum() {
 
     deviceNum = deviceNumber.value;
     console.log(deviceNum);
 
     dbRef.on('value', snap => {
-        console.log(snap.val()[deviceNum]);
+        childName = snap.val()[deviceNum].name;
+        childRoom = snap.val()[deviceNum].room;
+        childStatus = snap.val()[deviceNum].status;
+        console.log(snap.val()[deviceNum].status);
     });
+    populateInfo();
+
 }
 
 
-
-console.log(dbRef);
-
-
+function populateInfo() {
+    var $infoTemplate = $('.card.template').clone();
+    $infoTemplate.find("#cardName").text(childName);
+    $infoTemplate.find("#cardRoom").text(childRoom);
+    $infoTemplate.find("#cardNum").text(deviceNum);
+    $infoTemplate.find("#cardStatus").text(childStatus);
+    $infoTemplate.removeClass("template");
+    $('#child_status').append($infoTemplate);
+}
 
 
 // const dbRef = firebase.database().ref().child('devices/' + deviceNum);
